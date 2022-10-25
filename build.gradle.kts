f89293e2b1c80bb2256d6f7401768735368d293b
DominikKossinski <dominikkossinski@gmail.com>
2022-10-25 21:30:40 +0200

    libs update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.0"
+    id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.7.0"
     kotlin("plugin.spring") version "1.7.0"
@@ -17,27 +17,27 @@ repositories {
 
 dependencies {
     //Firebase
-    implementation("com.google.firebase:firebase-admin:9.0.0")
+    implementation("com.google.firebase:firebase-admin:9.1.0")
 
     //Spring
-    implementation("org.springframework.boot:spring-boot-starter-web:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-security:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.4")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.7.4")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.4")
 
     //Validation
     implementation("javax.validation:validation-api:2.0.1.Final")
 
     //Mongo
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive:2.7.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.7.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive:2.7.4")
 
     //Swagger
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
-    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
-    implementation("org.springdoc:springdoc-openapi-security:1.6.9")
-    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
+    implementation("org.springdoc:springdoc-openapi-security:1.6.12")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
 
     //Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -47,7 +47,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
 
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.2") {
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.4") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
 
